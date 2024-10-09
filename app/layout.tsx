e0ed7b95aa330658134c1400ec25099028457e91
Ninad Kulkarni <ninadk03@gmail.com>
2024-10-09 16:05:19 -0700

    Added custom colors and fixed font

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`${inter.className} font-sans`}>{children}</body>
+            <body className={`${inter.variable} font-sans`}>{children}</body>
         </html>
     );
 }
