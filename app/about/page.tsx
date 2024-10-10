07360d19d742d99d87330fa6d121417b9ce177f1
Ninad Kulkarni <ninadk03@gmail.com>
2024-10-09 21:54:44 -0700

    Added route files

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function About() {
+    return (
+        <div>
+            <h1 className="text-4xl font-bold">About Us</h1>
+            <p>Our Mission</p>
+            <p>Our Team</p>
+            <p>Contact Us</p>
+        </div>
+    );
+}
---
+++ app/faq/page.tsx
@@ -0,0 +1,9 @@
+export default function FAQ() {
+    return (
+        <div>
+            <h1 className="text-4xl font-bold">Frequently Asked Questions</h1>
+            <p>Search bar</p>
+            <p>Use accordion</p>
+        </div>
+    );
+}
