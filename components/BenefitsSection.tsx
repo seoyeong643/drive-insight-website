d47961a0c85fa5a10da975b6e7535d4981423a7b
Rhea Mane <95063614+orangejuice001@users.noreply.github.com>
2024-10-22 21:37:27 -0700

    Component - Benefits Section - Responsive Complete #37

--- components/BenefitsSection.tsx
+++ components/BenefitsSection.tsx
@@ -3,9 +3,9 @@ import BenefitCard from "./BenefitCard";
 
 export const BenefitsSection = () => {
     return (
-        <div>
-            <h1>Benefits Section</h1>
-            <div className="flex flex-col items-center gap-4 p-4">
+        <div className="container mx-auto flex flex-col gap-5 px-6">
+            <h2 className="text-center">Get more out of your drive</h2>
+            <div className="flex flex-col items-center gap-4 md:flex-row md:gap-8">
                 <BenefitCard
                     title="Fuel Consumption and Cost"
                     description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
@@ -17,6 +17,7 @@ export const BenefitsSection = () => {
                     description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
                     icon={<ShieldCheck className="h-10 w-10" />}
                     imgPath={"/benefit_safety_1.webp"}
+                    darkMode={false}
                 />
                 <BenefitCard
                     title="Privacy Control"
